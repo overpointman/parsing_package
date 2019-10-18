baef03460c3c5ac8a167234391903c51a3aef5d0
yunkuangao <b412034066@163.com>
2019-10-18 16:40:39 +0800

    单独使用测试类

--- src/main/java/utils/Test.java
+++ src/main/java/utils/Test.java
@@ -21,9 +21,20 @@ public class Test {
 
     public static void main(String[] args) throws DocumentException, IOException, IllegalAccessException, InstantiationException {
         String xml = FileTool.readStringFromFile(FILE_NAME, "UTF-8");
+
+        long startTime = System.currentTimeMillis();   //获取开始时间
         List domToMap = DomToMap(xml);
         System.out.println("xml to map:" + domToMap);
+        long endTime = System.currentTimeMillis(); //获取结束时间
+        System.out.println("程序运行时间： " + (endTime - startTime) + "ms");
+
+        long startTime2 = System.currentTimeMillis();   //获取开始时间
         String mapToDom = MapToDom(domToMap);
         System.out.println("map to xml:" + mapToDom);
+        long endTime2 = System.currentTimeMillis(); //获取结束时间
+        System.out.println("程序运行时间： " + (endTime2 - startTime2) + "ms");
+
+
+
     }
 }
